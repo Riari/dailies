b1782690cfee121511864437e726bac321686261
Riari <3583774+Riari@users.noreply.github.com>
2019-07-27 22:10:26 +0100

    Integrate preact-router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,76 +1,19 @@
-import { Component } from 'preact';
 import { Provider } from 'unistore/preact';
-import { Menu, PlusCircle, X } from 'react-feather';
+import Router from 'preact-router';
 
 import './style';
 import store from './store';
-import TaskList from './components/TaskList';
-import Task from './models/Task';
 
-export default class App extends Component {
-	render() {
-		return (
-			<Provider store={store}>
-				<div class="wrap">
-					<div class="side">
-						<div class="taskbar">
-							<X />
-						</div>
-						<div class="content">
-							<div class="section">
-								<h2>Lists</h2>
-								<ul>
-									<li class="active">Work</li>
-									<li>Side project</li>
-									<li>Fitness</li>
-								</ul>
-							</div>
-							<div class="section stats">
-								<h2>Stats</h2>
-								<div class="item">
-									<span>Level <strong>43</strong></span>
-									<div class="bar">
-										<div class="background"></div>
-										<div class="fill" style="width: 65%"></div>
-									</div>
-								</div>
-								<div class="item">
-									<span>XP earned (today) <strong>500</strong></span>
-								</div>
-								<div class="item">
-									<span>XP earned (lifetime) <strong>35,250</strong></span>
-								</div>
-							</div>
-							<div class="section records">
-								<h2>Records</h2>
-								<div class="item">
-									<span>Longest streak <strong>9 days</strong></span>
-								</div>
-								<div class="item">
-									<span>Most completions in a day <strong>6 tasks</strong></span>
-								</div>
-							</div>
-						</div>
-					</div>
-					<div class="main">
-						<div class="hero">
-							<div class="taskbar">
-								<Menu />
-							</div>
-							<h1>Dailies</h1>
-						</div>
-						<div class="tasks">
-							<h2>Incomplete</h2>
-							<button class="action">
-								<PlusCircle /> New task
-							</button>
-							<TaskList status={Task.STATUS_INCOMPLETE} />
-							<h2>Completed Today</h2>
-							<TaskList status={Task.STATUS_COMPLETED} />
-						</div>
-					</div>
-				</div>
-			</Provider>
-		);
-	}
+import Home from './components/Home';
+
+const App = () => {
+	return (
+		<Provider store={store}>
+			<Router>
+				<Home path="/" />
+			</Router>
+		</Provider>
+	);
 }
+
+export default App;
